644fc546fd56d15a7d016eb38ae38d5ceecb6e1f
Cyrus Kashef <43621358+CyrusKashef@users.noreply.github.com>
2022-03-22 15:20:14 -0500

    Cheat Sheet
    Always generates the cheat sheet

--- Randomization_Processes/Misc_Manipulation/Models_Animations_Properties/Models_Animations_Properties_Main.py
+++ Randomization_Processes/Misc_Manipulation/Models_Animations_Properties/Models_Animations_Properties_Main.py
@@ -10,11 +10,10 @@ from Randomization_Processes.Misc_Manipulation.Models_Animations_Properties.Anim
 from ...Common_Functions import read_json
 
 class Models_Animations_Properties_Class():
-    def __init__(self, seed_val, file_dir, randomized_rom_path, selected_json, cheat_sheet=0):
+    def __init__(self, seed_val, file_dir, randomized_rom_path, selected_json):
         self.seed_val = seed_val
         self.file_dir = file_dir
         self.randomized_rom_path = randomized_rom_path
-        self.cheat_sheet = cheat_sheet
         self._master_dict = read_json(f"{self.file_dir}Randomization_Processes/Misc_Manipulation/Models_Animations_Properties/{selected_json}.json")
     
     def _models_main(self):
@@ -31,5 +30,4 @@ class Models_Animations_Properties_Class():
         properties_dict = self._master_dict["Properties"]
         properties_manip_obj = Properties_Manipulation_Class(self.seed_val, self.file_dir, properties_dict)
         properties_manip_obj._swap_properties_main()
-        if(self.cheat_sheet == 1):
-            properties_manip_obj._generate_cheat_sheet()
+        properties_manip_obj._generate_cheat_sheet()
