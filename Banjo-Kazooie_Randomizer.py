2ecb8b1df69a8814f3b64320287c8d6f063470c7
Cyrus Kashef <43621358+CyrusKashef@users.noreply.github.com>
2022-03-29 23:57:23 -0500

    Version is now set here
    Easier for me to find

--- Banjo-Kazooie_Randomizer.py
+++ Banjo-Kazooie_Randomizer.py
@@ -10,10 +10,16 @@ Created on Nov 5, 2021
 
 from User_GUI import User_GUI_Class
 
+#################
+### VARIABLES ###
+#################
+
+BK_RANDO_VERSION = "2.0.20220329"
+
 ############
 ### MAIN ###
 ############
 
 if __name__ == '__main__':
-    user_app = User_GUI_Class()
+    user_app = User_GUI_Class(BK_RANDO_VERSION)
     user_app._main()
